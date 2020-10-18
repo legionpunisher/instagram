c6193d21ac9d50b7ab75b86ed598c3c2084ce38f
legionpunisher <mbuguamoses656@gmail.com>
2020-10-18 19:56:47 +0300

    added an if settings debug in our app url

--- falcon/urls.py
+++ falcon/urls.py
@@ -18,5 +18,7 @@ urlpatterns = [
     url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         views.activate, name='activate'),
     url(r'^follow/(?P<operation>.+)/(?P<id>\d+)',views.follow,name='follow'),
-
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
