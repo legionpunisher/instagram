09fae6ea449683d7e5024c979163067ba7976437
legionpunisher <mbuguamoses656@gmail.com>
2020-10-18 19:53:17 +0300

    crreated a url path for our searchand views

--- falcon/urls.py
+++ falcon/urls.py
@@ -2,3 +2,9 @@ from django.conf.urls import url
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+
+urlpatterns = [
+    url(r'^$', views.home,name='home'),
+    url(r'^accounts/profile/', views.add_profile, name='add_profile'),
+    url(r'^profile/(\d+)', views.profile, name='profile'),
+    url(r'^search/', views.search_results, name='search_results'),
