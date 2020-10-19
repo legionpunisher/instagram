38985a9a8031e4dc8d7e681dcbd77386b29a7569
legionpunisher <mbuguamoses656@gmail.com>
2020-10-19 05:52:41 +0300

    modified our model to have the image and profile class

--- falcon/models.py
+++ falcon/models.py
@@ -1,63 +1,85 @@
 from django.db import models
 from django.contrib.auth.models import User
+from tinymce.models import HTMLField
 
 # Create your models here.
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE,null=True)
-    profile_photo= models.ImageField(upload_to='profiles/',null=True)
-    bio= models.CharField(max_length=240, null=True)
+    name = models.ForeignKey(User,on_delete=models.CASCADE)
+    profile_photo = models.ImageField(upload_to ='images/')
+    bio = models.TextField()
+
+    def __str__(self):
+        return self.name.username
 
     def save_profile(self):
         self.save()
 
     @classmethod
-    def get_profile(cls):
-        profile = Profile.objects.all()
+    def get_profile(cls,identity):
+        profile = Profile.objects.filter(name__username__icontains = identity)
         return profile
-    @classmethod
-    def find_profile(cls,search_term):
-        profile = Profile.objects.filter(user__username__icontains=search_term)
-        return profile
-class Image(models.Model):
-    posted_by = models.ForeignKey(User, null=True,on_delete=models.CASCADE)
-    profile = models.ForeignKey(Profile, on_delete=models.CASCADE,null=True)
-    insta_image = models.ImageField(upload_to='picha/',null=True)
-    caption = models.TextField(null=True)
-    likes = models.PositiveIntegerField(default=0)
 
     @classmethod
-    def get_images(cls):
-        images = Image.objects.all()
-        return images
+    def delete_profile(cls,id):
+        prof= Profile.objects.filter(id = id)
+        prof.delete()
 
-    def __str__(self):
-       return str(self.caption)
+    @classmethod
+    def search_profile(cls,jina):
+        profile = Profile.objects.filter(name__username__icontains=jina)
+        return profile
 
-class Comment(models.Model):
-    poster = models.ForeignKey(User, on_delete=models.CASCADE,null=True)
-    image = models.ForeignKey(Image, on_delete=models.CASCADE, related_name='comments',null=True)
-    comment = models.CharField(max_length=200, null=True)
+class Like(models.Model):
+    likes = models.IntegerField(null = True)
 
-    def __str__(self):
-        return self.comment
+    def __int__(self):
+        return self.likes
 
     def save_comment(self):
         self.save()
 
-    @classmethod
-    def get_comment(cls):
-        comment = Comment.objects.all()
-        return comment
-class Follow(models.Model):
-    users=models.ManyToManyField(User,related_name='follow')
-    current_user=models.ForeignKey(User,related_name='c_user',null=True,on_delete=models.CASCADE)
+class Comment(models.Model):
+    comments =models.CharField(max_length= 90,blank= True)
+    post_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey('app.Image', on_delete=models.CASCADE, related_name='opinions')
+
+    def __str__(self):
+        return self.comments
+
+    def save_comment(self):
+        self.save()
+
+class Image(models.Model):
+    image = models.ImageField(upload_to = 'images/')
+    image_name = models.CharField(max_length = 60)
+    image_caption = models.CharField(max_length = 60)
+    profile = models.ForeignKey(Profile)
+    likes = models.ForeignKey(Like)
+    comments = models.ForeignKey(Comment,on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return self.image_name
+
+    def save_image(self):
+        self.save()
 
     @classmethod
-    def follow(cls,current_user,new):
-        friends,created=cls.objects.get_or_create(current_user=current_user)
-        friends.users.add(new)
+    def get_image(cls,id):
+        image = Image.objects.filter(id = id)
+        return image
 
     @classmethod
-    def unfollow(cls,current_user,new):
-        friends,created=cls.objects.get_or_create(current_user=current_user)
-        friends.users.remove(new)
+    def get_post(cls,jina):
+        images = Image.objects.filter(profile__name__username__icontains = jina)
+        return images
+
+    @classmethod
+    def delete_image(cls,id):
+        image = Image.objects.filter(id = id)
+        image.delete()
+
+    @classmethod
+    def update_caption(cls,id,cap):
+        image = Image.objects.filter(id=id)
+        image.update(image_caption = cap)
+        return image
--- falcon/urls.py
+++ falcon/urls.py
@@ -1,24 +1,17 @@
 from django.conf.urls import url
-from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from . import views
 
 urlpatterns = [
-    url(r'^$', views.home,name='home'),
-    url(r'^accounts/profile/', views.add_profile, name='add_profile'),
-    url(r'^profile/(\d+)', views.profile, name='profile'),
-    url(r'^search/', views.search_results, name='search_results'),
-    url(r'^image/(\d+)',views.get_image_by_id,name ='image'),
-    url(r'^new/profile$', views.add_profile, name='add_profile'),
-    url(r'^upload/', views.update_image, name='upload'),
-    url(r'^comment/(?P<pk>\d+)',views.add_comment,name='comment'),
+    url('^$',views.home,name = 'home'),
+    url(r'^picture/(\d+)',views.image,name = 'picture'),
+    url(r'^profile/(\w+)',views.profile,name = 'profile'),
+    url(r'^edit/profile$',views.new_profile,name ='edit_profile'),
+    url(r'^new/comment/(\d+)',views.comment,name = 'new_comment'),
+    url(r'new/image$',views.post,name='new_post'),
+    url(r'^search/',views.search,name = 'search'),
     url(r'^like/(?P<operation>.+)/(?P<pk>\d+)',views.like, name='like'),
-    url(r'^all/(?P<pk>\d+)', views.all, name='all'),
-    url(r'^signup/$', views.signup, name='signup'),
-    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-        views.activate, name='activate'),
-    url(r'^follow/(?P<operation>.+)/(?P<id>\d+)',views.follow,name='follow'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-
